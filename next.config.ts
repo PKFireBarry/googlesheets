014ac7fe9bb41580c6fbc796de2eb27df639feb7
Darion George <Barry0719@gmail.com>
2025-02-17 18:24:03 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['kg.diffbot.com'],
+  },
 };
 
 export default nextConfig;
